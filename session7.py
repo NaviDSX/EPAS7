db7c03f3548f162ffa9d878d97ec4c55a355af38
NaviDSX <74872261+NaviDSX@users.noreply.github.com>
2021-06-27 04:29:23 +0530

    All Tests Passed

--- session7.py
+++ session7.py
@@ -1,13 +1,13 @@
 from collections import Callable
 
 
-def scanfunc(scanlen: int) -> 'Function':
+def scanfunc(scanlen: int) -> 'function':
     """
     Closure that takes a function and then check whether the function passed has a docstring with more than 50 characters. 50 is stored as a free variable.
     """
-    def doclength(fn: 'Function') -> 'True or False':
+    def doclength(fn: 'function') -> 'True or False':
         """
-        Inner Function that checks the docstring length.
+        Inner function that checks the docstring length.
         """
         if not isinstance(fn, Callable):
             raise TypeError("Expected function!")
@@ -16,15 +16,15 @@ def scanfunc(scanlen: int) -> 'Function':
     return doclength
 
 
-def fibonacci() -> "Function":
+def fibonacci() -> "function":
     """
-    Function that uses closure and gives you the next Fibonacci number.
+    function that uses closure and gives you the next Fibonacci number.
     Tracks the old Fibonacci number using free variable.
     """
     n1, n2 = 1, 1
     count = 0
 
-    def fibonaccinext() -> "Number":
+    def fibonaccinext():
         """
         Returns the next fibonacci number in the sequence.
         """
@@ -40,13 +40,13 @@ def fibonacci() -> "Function":
 func_count = {}
 
 
-def counter() -> "Function":
+def counter() -> "function":
     """
-    Counter Function to count the number using free variables
+    Counter function to count the number using free variables
     """
     counter = {}
 
-    def count(func: 'Function', *args, **kwargs):
+    def count(func: 'function', *args, **kwargs):
         """
         This function updates and keeps track of the number of times, 
         a function might be called.
@@ -59,15 +59,15 @@ def counter() -> "Function":
     return count
 
 
-def counter_users(user_dict: dict) -> "Function":
+def counter_users(user_dict: dict) -> "function":
     """
-    Function to count users using free variables.
+    function to count users using free variables.
     """
     counter = {}
     if not isinstance(user_dict, dict):
         raise TypeError('Expected Dictionary')
 
-    def count(func: 'Function', *args, **kwargs):
+    def count(func: 'function', *args, **kwargs):
         """
         This function updates and keeps track of the number of times, 
         a function might be called.
--- test_session7.py
+++ test_session7.py
@@ -1,5 +1,5 @@
 import pytest
-from numpy.random import randint
+import random
 import session7
 import os
 import inspect
@@ -90,114 +90,157 @@ def test_function_annotations():
     for function in functions:
         assert function[1].__annotations__
 
+# 1. ScanFunc (4 Cases)
 
-def test_doc_checker():
+#1a
+def test_doc_checker_scan():
     dcheck = session7.scanfunc(50)
-    assert dcheck(session7.counter) == True, "Check the doc_check function for scan."
-
+    assert dcheck(session7.scanfunc) == True, "Check the doc_check function for scan."
+#1b
 def test_doc_checker_fib():
-    dcheck = session7.fibonacci()
-    assert dcheck(fibonacci) == True, "Check the doc_check function for fib"
-
-
+    dcheck = session7.scanfunc(50)
+    assert dcheck(session7.fibonacci) == True, "Check the doc_check function for fib"
+#1c
+def test_doc_checker_func():
+    dcheck = session7.scanfunc(50)
+    assert dcheck(session7.counter) == True, "Check the doc_check function for func"
+#1d
 def test_doc_checker_parameter_type():
     with pytest.raises(TypeError):
         dc = session7.scanfunc()
         dc("No input")
 
-
-def test_fibonacci():
+# 2. Fibonacci (2 Cases)
+#2a
+def test_fibonacci_boundary():
     fib = session7.fibonacci()
-    assert fibonacci() == 1, "Check Fibonacci Function"
-    assert fibonacci() == 1, "Check Fibonacci Function"
-    assert fibonacci() == 2, "Check Fibonacci Function"
+    assert fib() == 1, "Check Fibonacci Function"
+    assert fib() == 1, "Check Fibonacci Function"
+    assert fib() == 2, "Check Fibonacci Function"
+#2b
+def test_fibonacci_random():
+    # Checking for 11th fibonacci number
     fib = session7.fibonacci()
     for i in range(10):
-        fibonacci()
-    # Checking for 11th fibonacci number
-    assert fibonacci() == 89, "Check Fibonacci Function"
+        fib()
+    assert fib() == 89, "Check Fibonacci Function"
 
 
-def test_func_count():
+# 3. Function Counter (6 Cases)
+#3a
+def test_func_add_count():
     def add(a, b):
         return a+b
-
-    def mul(a, b):
-        return a*b
-
-    def div(a, b):
-        return b and a/b
     fc = session7.counter()
     for _ in range(10):
         fc(add, 2, 5)
+    assert session7.func_count['add'] == 10, "Check the func_count function."
+
+#3b
+def test_func_mul_count():
+    def mul(a, b):
+        return a*b
+    fc = session7.counter()
     for _ in range(7):
         fc(mul, 2, 5)
+    assert session7.func_count['mul'] == 7, "Check the func_count function."
+
+#3c
+def test_func_div_count():
+    def div(a, b):
+        return b and a/b
+    fc = session7.counter()
     for _ in range(15):
         fc(div, 2, 5)
+    assert session7.func_count['div'] == 15, "Check the func_count function."
+
+#3d
+def test_func_print_count():
+    fc = session7.counter()
     for _ in range(3):
         fc(print, "Apple")
-
-    assert session7.func_count['add'] == 10, "Check the func_count function."
-    assert session7.func_count['mul'] == 7, "Check the func_count function."
-    assert session7.func_count['div'] == 15, "Check the func_count function."
     assert session7.func_count['print'] == 3, "Check the func_count function."
 
-
+#3e
 def test_func_count_parameter_type():
     with pytest.raises(TypeError):
         fc = session7.counter()
         fc("This is not a function and should throw error!")
+#3f
 
+# 4. Custom Function Counter (6 Cases)
 
-def test_func_count_custom():
+#4a
+def test_func_count_add_custom():
     def add(a, b):
         return a+b
-
-    def mul(a, b):
-        return a*b
-
-    def div(a, b):
-        return b and a/b
-
     dict_1 = dict()
     fc = session7.counter_users(dict_1)
-
-    a, b, c, d = randint(1, 10, 4)
-
+    a, b, c, d = random.randint(1, 10),random.randint(1, 10),random.randint(1, 10),random.randint(1, 10)
     for _ in range(a):
         fc(add, 2, 5)
+    assert dict_1['add'] == a, "Check the func_count function."
+
+    dict_2 = dict()
+    fc_new = session7.counter_users(dict_2)
+    a, b, c, d = random.randint(1, 10),random.randint(1, 10),random.randint(1, 10),random.randint(1, 10)
+    for _ in range(a):
+        fc_new(add, 2, 5)
+    assert dict_2['add'] == a, "Check the func_count function."
+
+#4b
+def test_func_count_mul_custom():
+    def mul(a, b):
+        return a*b
+    dict_1 = dict()
+    fc = session7.counter_users(dict_1)
+    a, b, c, d = random.randint(1, 10),random.randint(1, 10),random.randint(1, 10),random.randint(1, 10)
     for _ in range(b):
         fc(mul, 2, 5)
+    assert dict_1['mul'] == b, "Check the func_count function."
+
+    dict_2 = dict()
+    fc_new = session7.counter_users(dict_2)
+    a, b, c, d = random.randint(1, 10),random.randint(1, 10),random.randint(1, 10),random.randint(1, 10)
+    for _ in range(b):
+        fc_new(mul, 2, 5)
+    assert dict_2['mul'] == b, "Check the func_count function."
+
+#4c
+def test_func_count_div_custom():
+    def div(a, b):
+        return b and a/b
+    dict_1 = dict()
+    fc = session7.counter_users(dict_1)
+    a, b, c, d = random.randint(1, 10),random.randint(1, 10),random.randint(1, 10),random.randint(1, 10)
     for _ in range(c):
         fc(div, 2, 5)
+    assert dict_1['div'] == c, "Check the func_count function."
+
+    dict_2 = dict()
+    fc_new = session7.counter_users(dict_2)
+    a, b, c, d = random.randint(1, 10),random.randint(1, 10),random.randint(1, 10),random.randint(1, 10)
+    for _ in range(c):
+        fc_new(div, 2, 5)
+    assert dict_2['div'] == c, "Check the func_count function."
+
+#4d
+def test_func_count_print_custom():
+    dict_1 = dict()
+    fc = session7.counter_users(dict_1)
+    a, b, c, d = random.randint(1, 10),random.randint(1, 10),random.randint(1, 10),random.randint(1, 10)
     for _ in range(d):
         fc(print, "Apple")
-
-    assert dict_1['add'] == a, "Check the func_count function."
-    assert dict_1['mul'] == b, "Check the func_count function."
-    assert dict_1['div'] == c, "Check the func_count function."
     assert dict_1['print'] == d, "Check the func_count function."
 
     dict_2 = dict()
     fc_new = session7.counter_users(dict_2)
-
-    a, b, c, d = randint(1, 10, 4)
-
-    for _ in range(a):
-        fc_new(add, 2, 5)
-    for _ in range(b):
-        fc_new(mul, 2, 5)
-    for _ in range(c):
-        fc_new(div, 2, 5)
+    a, b, c, d = random.randint(1, 10),random.randint(1, 10),random.randint(1, 10),random.randint(1, 10)
     for _ in range(d):
         fc_new(print, "Apple")
-
-    assert dict_2['add'] == a, "Check the func_count function."
-    assert dict_2['mul'] == b, "Check the func_count function."
-    assert dict_2['div'] == c, "Check the func_count function."
     assert dict_2['print'] == d, "Check the func_count function."
 
-
+#4e
 def test_func_count_custom_parameter_type():
     with pytest.raises(TypeError):
         fc = session7.counter_users(1)
